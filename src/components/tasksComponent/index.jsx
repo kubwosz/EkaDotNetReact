0d3c0884dd83f975bcb8feafeeffc97c3310558a
jakub.woszczyna <woszczyna.jakub@gmail.com>
2019-05-28 07:14:03 +0200

    added adding and deleting methods and some csss

--- src/components/tasksComponent/index.jsx
+++ src/components/tasksComponent/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Form } from "react-bootstrap";
 import "./style.css";
 import axios from "axios";
 
@@ -7,11 +7,17 @@ export default class TasksComponent extends React.Component {
   constructor() {
     super();
     this.state = {
-      tasks: []
+      tasks: [],
+      newTaskName: "",
+      newTaskDescription: ""
     };
   }
 
   componentDidMount() {
+    this.getTasks();
+  }
+
+  getTasks() {
     axios.get("/todo").then(res => {
       console.log(res);
       this.setState({
@@ -20,19 +26,94 @@ export default class TasksComponent extends React.Component {
     });
   }
 
-  render() {
+  addNewTask = task => {
+    axios
+      .post("/todo", {
+        name: this.state.newTaskName,
+        description: "fsaf"
+      })
+      .then(() => {
+        this.getTasks();
+      });
+  };
+
+  deleteTask = id => {
+    axios.delete("/todo", {
+      data: {
+        id: 3
+      }
+    });
+  };
+
+  onChange = e => {
+    console.log(e.target);
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  isFormFilled = () => {
+    if (this.state.newTaskName !== "" && this.state.newTaskDescription !== "") {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  renderCard = (task, k) => {
     return (
-      <Card style={{ width: "18rem" }}>
+      <Card key={k} className="task-item">
         <Card.Body>
-          <Card.Title>
-            {this.state.tasks[0] ? this.state.tasks[0].name : null}
-          </Card.Title>
-          <Card.Text>
-            {this.state.tasks[0] ? this.state.tasks[0].description : null}
-          </Card.Text>
-          <Button variant="primary">Kliknij</Button>
+          <Card.Title>{task.name}</Card.Title>
+          <Card.Text>{task.description}</Card.Text>
         </Card.Body>
       </Card>
+    );
+  };
+
+  render() {
+    const tasks = this.state.tasks
+      .slice(0)
+      .reverse()
+      .map((val, index) => {
+        return this.renderCard(val, index);
+      });
+
+    return (
+      <div id="tasksPage">
+        <div id="tasks">{tasks}</div>
+        <div id="newTask">
+          <h1>Add new task:</h1>
+          <Form id="newTaskForm">
+            <Form.Group>
+              <Form.Label>Name:</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                name="newTaskName"
+                onChange={this.onChange}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>Description:</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows="3"
+                name="newTaskDescription"
+                onChange={this.onChange}
+                required
+              />
+            </Form.Group>
+            <Button
+              variant="primary"
+              disabled={!this.isFormFilled()}
+              onClick={this.addNewTask}
+            >
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </div>
     );
   }
 }
